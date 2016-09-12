2d7ccf6faf7fb6e998532c41ff32b019c3f5f627
Precious Opusunju <haxorpreshy@gmail.com>
2016-09-12 20:19:40 +0100

    Update app.js
    Added Validation.

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
+function validate() {
+	var valid = true;
+	var usern = $('#usern').val().length;
+	if(usern < 6 ) {
+		$('#load').html('Username is short!');
+		valid = false;
+	}
+	$('#usern').keydown(function(e) {
+      if (e.shiftKey || e.ctrlKey || e.altKey) {
+        e.preventDefault();
+      } else {
+        var key = e.keyCode;
+        if (!((key == 8) || (key == 32) || (key == 46) || (key >= 35 && key <= 40) || (key >= 65 && key <= 90))) {
+          e.preventDefault();
+        }
+      }
+    });
+	return valid;
+}
+
 function checkusername(){
+	var valid = validate();
+	if(valid) {
 	var usern = $('#usern').val();
 	$.ajax({
 		url: "app.php",
@@ -11,4 +33,5 @@ function checkusername(){
           $('#load').html(data);
 		}
 	});
+}
 }
